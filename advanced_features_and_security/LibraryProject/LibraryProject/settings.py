c34828f92c31d1e66aad143cb7928d4e367c288e
Hillary Robert <roberthillary30@gmail.com>
2025-11-18 18:20:45 +0200

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -45,10 +45,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "bookshelf",
     'relationship_app',
-    "accounts",
+    
 ]
 
-AUTH_USER_MODEL = "accounts.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
+
 
 
 
